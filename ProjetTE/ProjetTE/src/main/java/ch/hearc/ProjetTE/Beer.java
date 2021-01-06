5611e9a0cb8de2d3c11a7cc7a1af25eccab16a73
Monnet Joris <joris.monnet@he-arc.ch>
2021-01-06 19:25:46 +0100

    make it works

--- ProjetTE/ProjetTE/src/main/java/ch/hearc/ProjetTE/Beer.java
+++ ProjetTE/ProjetTE/src/main/java/ch/hearc/ProjetTE/Beer.java
@@ -13,6 +13,7 @@ public class Beer {
 		GARDE, ABBAYE, LAGER, PALE_ALE, INDIA_PALE_ALE, STOUT, BLANCHE, SAISON, SOUR, FUT, INCONNUE
 	}
 
+	private int index;
 	private int stock;
 	private String name;
 	private String brand;
@@ -31,5 +32,77 @@ public class Beer {
 		this.color = color;
 		this.fermentation = fermentation;
 		this.style = style;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public ColorType getColor() {
+		return color;
+	}
+
+	public void setColor(ColorType color) {
+		this.color = color;
+	}
+
+	public FermentationType getFermentation() {
+		return fermentation;
+	}
+
+	public void setFermentation(FermentationType fermentation) {
+		this.fermentation = fermentation;
+	}
+
+	public StyleType getStyle() {
+		return style;
+	}
+
+	public void setStyle(StyleType style) {
+		this.style = style;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+	
+	public void setIndex(int index) {
+		this.index = index;
 	}
 }
--- ProjetTE/ProjetTE/src/main/java/ch/hearc/ProjetTE/BeerController.java
+++ ProjetTE/ProjetTE/src/main/java/ch/hearc/ProjetTE/BeerController.java
@@ -1,9 +1,12 @@
 package ch.hearc.ProjetTE;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,34 +20,39 @@ import ch.hearc.ProjetTE.Beer.ColorType;
 import ch.hearc.ProjetTE.Beer.FermentationType;
 import ch.hearc.ProjetTE.Beer.StyleType;
 
-@RestController
+@Controller
+@RequestMapping("/bieres")
 public class BeerController {
-
-	private Map<Integer,Beer> mapBeer = new HashMap<>() {
-		private static final long serialVersionUID = 1L;
+	
+	private static List<Beer> listBeer = new ArrayList<>();
+	static {
+		listBeer.add(new Beer(5,"Nom","Heineken",4.5,0.33,ColorType.BLONDE,FermentationType.EN_BOUTEILLE,StyleType.SOUR));
+		listBeer.get(0).setIndex(0);
+	}
+	/* {
 		{
-			put(0,new Beer(5,"Nom","Heineken",4.5,0.33,ColorType.BLONDE,FermentationType.EN_BOUTEILLE,StyleType.SOUR));
-			put(1,new Beer(10,"Nom","Heineken",5,0.33,ColorType.AMBREE,FermentationType.HAUTE,StyleType.ABBAYE));
-			put(2,new Beer(1,"Nom","Heineken",3.5,0.33,ColorType.BLONDE,FermentationType.MIXTE,StyleType.FUT));
-			put(3,new Beer(2,"Nom","Heineken",4.5,0.5,ColorType.BLANCHE,FermentationType.SPONTANEE,StyleType.BLANCHE));
-			put(4,new Beer(50,"Nom","Heineken",2.5,0.25,ColorType.BRUNE,FermentationType.BASSE,StyleType.PALE_ALE));
-			put(5,new Beer(64,"Nom","Heineken",10,1,ColorType.BRUNE,FermentationType.EN_BOUTEILLE,StyleType.STOUT));
+			add(new Beer(5,"Nom","Heineken",4.5,0.33,ColorType.BLONDE,FermentationType.EN_BOUTEILLE,StyleType.SOUR).setIndex(0));
+			put(1,new Beer(10,"Nom","Heineken",5,0.33,ColorType.AMBREE,FermentationType.HAUTE,StyleType.ABBAYE).setIndex(0));
+			put(2,new Beer(1,"Nom","Heineken",3.5,0.33,ColorType.BLONDE,FermentationType.MIXTE,StyleType.FUT).setIndex(0));
+			put(3,new Beer(2,"Nom","Heineken",4.5,0.5,ColorType.BLANCHE,FermentationType.SPONTANEE,StyleType.BLANCHE).setIndex(0));
+			put(4,new Beer(50,"Nom","Heineken",2.5,0.25,ColorType.BRUNE,FermentationType.BASSE,StyleType.PALE_ALE).setIndex(0));
+			put(5,new Beer(64,"Nom","Heineken",10,1,ColorType.BRUNE,FermentationType.EN_BOUTEILLE,StyleType.STOUT).setIndex(0));
 		}
-	};
+	};*/
 	
 	private String notificationMessage;
 	
-	@GetMapping("/bieres")
-	public void showBeers(Model model) {
+	@GetMapping
+	public String showBeers(Model model) {
 		model.addAttribute("notification",notificationMessage);
-		model.addAttribute("beers",mapBeer.values());
-		//return "bieres";
+		model.addAttribute("beers",listBeer);
+		return "Beers";
 	}
 	
-	@GetMapping("/bieres/{id}")
-	public void showBeer(Model model,@PathVariable("id") int id){
-		model.addAttribute("beer",mapBeer.get(id));
-		
+	@GetMapping("/{id}")
+	public String showBeer(Model model,@PathVariable("id") int id){
+		model.addAttribute("beer",listBeer.get(id));
+		return "BeerDetail";
 	}
 	@PostMapping
 	public void createbeer(@RequestParam int i) {
@@ -53,7 +61,7 @@ public class BeerController {
 	
 	@DeleteMapping(value = "/{id}/delete")
 	public String deleteBeer(@PathVariable("id") int id) {
-		if(mapBeer.remove(id)!=null) {
+		if(listBeer.remove(id)!=null) {
 			notificationMessage = "Beer Deleted";
 		} else {
 			notificationMessage = "Beer"+ id +"doesn't exist";
